369949f493adcb28e3e0323a592aa1e3d24f1d08
deltathrs <deltatherof@gmail.com>
2023-12-13 18:54:39 +0700

    Billing Problem

--- lib/pages/service_list/carry_stuff.dart
+++ lib/pages/service_list/carry_stuff.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+import 'package:uuid/uuid.dart';
+import 'package:http/http.dart' as http;
 
 class CarryFeature extends StatefulWidget {
   const CarryFeature({super.key});
@@ -10,7 +14,14 @@ class CarryFeature extends StatefulWidget {
 }
 
 class _CarryFeatureState extends State<CarryFeature> {
-  Location _locationController = new Location();
+  // ignore: unused_field
+  TextEditingController _controller = TextEditingController();
+  var uuid = Uuid();
+  String _sessionToken = '12234';
+  // ignore: unused_field
+  List<dynamic> _placesList = [];
+
+  Location _locationController = Location();
 
   static const LatLng _pGooglePlex = LatLng(-6.200000, 106.816666);
   static const LatLng _pApplePark = LatLng(-6.1991814, 106.8145524);
@@ -20,6 +31,41 @@ class _CarryFeatureState extends State<CarryFeature> {
   void initState() {
     super.initState();
     getLocationUpdates();
+
+    _controller.addListener(() {
+      onChange();
+    });
+  }
+
+  void onChange() {
+    // ignore: unnecessary_null_comparison
+    if (_sessionToken == null) {
+      setState(() {
+        _sessionToken = uuid.v4();
+      });
+    }
+
+    getSuggestion(_controller.text);
+  }
+
+  void getSuggestion(String input) async {
+    String PLACES_API_KEY = "AIzaSyCe0U04_t-ph4Jtg98crIQjqt9kB7U2QP0";
+    String baseURL =
+        'https://maps.googleapis.com/maps/api/place/autocomplete/json';
+    String request =
+        '$baseURL?input=$input&key=$PLACES_API_KEY&session_token=$_sessionToken';
+
+    var response = await http.get(Uri.parse(request));
+
+    print(response.body.toString());
+
+    if (response.statusCode == 200) {
+      setState(() {
+        _placesList = jsonDecode(response.body.toString())['predictions'];
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
   }
 
   @override
@@ -39,18 +85,38 @@ class _CarryFeatureState extends State<CarryFeature> {
         ),
         backgroundColor: Colors.red,
       ),
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(target: _pGooglePlex, zoom: 15),
-        markers: {
-          Marker(
-              markerId: MarkerId("_currentLocation"),
-              icon: BitmapDescriptor.defaultMarker,
-              position: _pGooglePlex),
-          Marker(
-              markerId: MarkerId("_currentLocation"),
-              icon: BitmapDescriptor.defaultMarker,
-              position: _pApplePark),
-        },
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Masukkan Lokasi Anda',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          Expanded(
+            child: GoogleMap(
+              initialCameraPosition:
+                  CameraPosition(target: _pGooglePlex, zoom: 15),
+              markers: {
+                Marker(
+                  markerId: MarkerId("_currentLocation"),
+                  icon: BitmapDescriptor.defaultMarker,
+                  position: _pGooglePlex,
+                ),
+                Marker(
+                  markerId: MarkerId("_currentLocation"),
+                  icon: BitmapDescriptor.defaultMarker,
+                  position: _pApplePark,
+                ),
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
